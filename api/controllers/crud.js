3945871b56a6847fbd0eceaefba851d5ba2bf08c
karthik11c <41610811+karthik11c@users.noreply.github.com>
2018-10-30 22:12:59 +0530

    added new endpoints backend(update,delete,display)

--- api/controllers/crud.js
+++ api/controllers/crud.js
@@ -17,22 +17,25 @@ module.exports = app; // for testing
 var cloudant = Cloudant({url: cloudant_url});
 
 module.exports = {
- addUser:addUser
+ addUser:addUser,
+ getAllUsersInfo:getAllUsersInfo,
+ findUserById:findUserById,
+ findUserByName:findUserByName,
+  UpdateUserInfoById:UpdateUserInfoById,
+  DeleteUserInfoById:DeleteUserInfoById
 };
 
-var dbname = 'kikik';
-var db;
+var dbname = 'cruise-booked11k';
+var db = cloudant.db.use(dbname);
+
+//1.NewUser
 
 function addUser(req,res){//adding user to db
        console.log('Cust Detailsk: ');
        var jsonobj = req.swagger.params.customer.value.customer;
         var customerDetails = JSON.stringify(jsonobj);
-        var dbname = 'cruise-booked11k';
+
         console.log('customer:'+customerDetails);
-
-        var db;
-
-            db = cloudant.db.use(dbname);
             db.insert(jsonobj);
             res.contentType('application/json');
             var id =  jsonobj.id;
@@ -46,4 +49,199 @@ function addUser(req,res){//adding user to db
                                     "price": price
                                    }
             }).end();
+};
+
+//2.findUserById
+
+function findUserById(req,res){
+    var uid = req.swagger.params.uid.value;
+    var v = {
+    "selector": {
+      "id": uid
+    }
+   }
+   db.find(v,function(err,result){
+   if(err){
+     throw err;
+   }else {
+     console.log(result.docs);
+   }
+   res.send(result.docs).end();
+ });
+
+};
+
+//3.findUserByName
+
+function findUserByName(req,res){
+  var name = req.swagger.params.Name.value;
+  var v = {
+  "selector": {
+    "name": name
+  }
+ }
+ db.find(v,function(err,result){
+ if(err){
+   throw err;
+ }else {
+   console.log(result.docs);
+ }
+ res.send(result.docs).end();
+});
+};
+
+//4.UpdateUserInfoByName
+
+function UpdateUserInfoByName(req,res){
+  var jsonobj = req.swagger.params.customer.value.customer;
+
+  var f = {
+     "selector":{
+       "_id":jsonobj._id,
+       "_rev":jsonobj._rev
+     }
+  }
+db.find(f,function(err,data){
+console.log(data.docs);
+ if(JSON.stringify(data.docs)!="[]"){
+    var v = {
+    "_id":jsonobj._id,
+    "_rev":jsonobj._rev,
+    "id":jsonobj.id,
+    "name":jsonobj.name,
+    "price":jsonobj.price
+  }
+
+   db.insert(v,function(err,result){
+    if(err){
+      throw err;
+      console.log('noooo');
+    }else{
+      console.log('yyyy');
+    db.find({"selector":{"_id":jsonobj._id}},function(err,result1){
+      if(err){
+        console.log('something went wrong...');
+      }else{
+        console.log('updated doc:');
+        console.log(result1.docs);
+        res.send(result1.docs).end();
+      }
+    });
+    }
+   });
+ }else{
+   console.log('the requested user with _id id not found...');
+ }
+});
+
+
+};
+
+//5.UpdateUserInfoById
+
+function UpdateUserInfoById(req,res){
+  var jsonobj = req.swagger.params.customer.value.customer;
+
+  var f = {
+     "selector":{
+       "_id":jsonobj._id,
+       "_rev":jsonobj._rev
+     }
+  }
+db.find(f,function(err,data){
+console.log(data.docs);
+ if(JSON.stringify(data.docs)!="[]"){
+    var v = {
+    "_id":jsonobj._id,
+    "_rev":jsonobj._rev,
+    "id":jsonobj.id,
+    "name":jsonobj.name,
+    "price":jsonobj.price
+  }
+
+   db.insert(v,function(err,result){
+    if(err){
+      throw err;
+      console.log('noooo');
+    }else{
+      console.log('yyyy');
+    db.find({"selector":{"_id":jsonobj._id}},function(err,result1){
+      if(err){
+        console.log('something went wrong...');
+      }else{
+        console.log('updated doc:');
+        console.log(result1.docs);
+        res.send(result1.docs).end();
+      }
+    });
+    }
+   });
+ }else{
+   console.log('the requested user with _id id not found...');
+ }
+});
+};
+
+//6.DeleteUserInfoById
+
+function DeleteUserInfoById(req,res){
+    var jsonobj = req.swagger.params;
+    console.log(jsonobj.Id.value);
+    console.log(jsonobj.Rev.value);
+
+   // var f = {
+   //  "selector":{
+   //    "_id":jsonobj.Id.value,
+   //    "_rev":jsonobj.Rev.value
+   //  }
+   // }
+
+   // db.find(f,function(err,result){
+   //    if(err){
+   //      console.log('sorry the requested doc is not exist in database...');
+   //    }else {
+       db.destroy(jsonobj.Id.value,jsonobj.Rev.value,function(err,data){
+         if(err){
+           console.log('something went wrong...');
+         }else {
+           console.log('deleted successfully...');
+         }
+       });
+
+
+      // }
+   // });
+};
+
+//7.DeleteUserInfoByName
+
+// function DeleteUserInfoByName(req,res){
+//
+//
+// };
+
+//8.getAllUsersInfoByName
+
+   function getAllUsersInfo(req,res){
+    var v = {
+    "selector": {
+      "_id": {
+         "$gt": "0"
+      }
+    }
+   }
+  db.find(v,function(err,result){
+  if(err){
+    throw err;
+  }else {
+    console.log(result.docs);
+  }
+  res.send(result.docs).end();
+});
+
+
+// db.list({include_docs:true},function (err, data) {
+//   console.log(err, data);
+//   res.send(data).end();
+// });
 };
